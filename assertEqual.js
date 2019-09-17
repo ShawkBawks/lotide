5fbe5b23f6a3312daf86d9fb81f4460d7581fb57
Logan Roberts <lroberts77@gmail.com>
2019-09-17 01:08:24 +0000

    Adjusted code with back ticks

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log("\u1F60A","\u1F60A","\u1F60A", "Assertion Passed: " + actual + " === " + expected);
+    return console.log(`\u1F60A, \u1F60A, \u1F60A, Assertion Passed:  ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    return console.log("\u1F92C", "\u1F92C", "\u1F92C", "Assertion failed: " + actual + " !== " + expected);
+    return console.log(`\u1F92C, \u1F92C, \u1F92C, Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
