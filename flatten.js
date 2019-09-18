14d429124bccdf1b8c79cca0c2ba7329a77754db
Logan Roberts <lroberts77@gmail.com>
2019-09-18 20:31:37 +0000

    Created a function to merge array within an array.

---
+++ flatten.js
@@ -0,0 +1,30 @@
+let eqArrays = function(a, b) {
+  if (a instanceof Array && b instanceof Array) {
+    if (a.length !== b.length)  //ensuring correct length
+      return false;
+    for (let i = 0; i < a.length; i++) //ensuring each element is equal
+      if (!eqArrays(a[i], b[i]))
+        return (false);
+    return (true);
+
+  } else {
+    return a === b;
+  }
+};
+
+let assertArraysEqual = function(a, b) {
+  if (eqArrays(a,b)) {
+    console.log("this array is true");
+  
+  } else {
+    console.log("this array is false");
+  }
+};
+
+let flatten = function(arr){
+  let arr2 = [arr];
+  let flattened = [].concat.apply([], arr);
+  
+  return flattened;
+}
+ console.log(flatten([1, 2, [3, 4], 5, [6]]));
