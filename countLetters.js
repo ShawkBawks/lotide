49dc3cd3b4d6eff11ca8df58dbf8a760c05e9bf4
Logan Roberts <lroberts77@gmail.com>
2019-09-18 22:26:32 +0000

    Created function that would count character occurences and implemented way to remove spaces from the quote.

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`\u1F60A, \u1F60A, \u1F60A, Assertion Passed:  ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return (`\u1F92C, \u1F92C, \u1F92C, Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+let quote = "Lighthouse Labs in the House of Light";
+quote = quote.replace(/\s+/g, '');  //removes spaces from string
+let countLetters = function(sentence) {
+  const result = {};
+
+  const letters = sentence.split ('');
+  for (let letter of letters){
+    letter = letter.toLowerCase();  //converts to lower case
+    if (result[letter]) {
+      result[letter] = result[letter] + 1; //if result is in the object it will add to total
+    } else {
+      result[letter] = 1; //if not in object it will add additional key
+    }
+  }
+  return result;
+};
+
+const letterCount = countLetters(quote);
+console.log(letterCount);
