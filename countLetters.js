745d7dc7346db81e7307f4e92f1df43d2f8eacc1
Logan Roberts <lroberts77@gmail.com>
2019-09-19 01:54:36 +0000

    Refactored code

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     return (`\u1F60A, \u1F60A, \u1F60A, Assertion Passed:  ${actual} === ${expected}`);
   } else if (actual !== expected) {
@@ -8,20 +8,20 @@ const assertEqual = function(actual, expected) {
 
 let quote = "Lighthouse Labs in the House of Light";
 quote = quote.replace(/\s+/g, '');  //removes spaces from string
-let countLetters = function(sentence) {
+let countLetters = function (sentence) {
   const result = {};
 
-  const letters = sentence.split ('');
-  for (let letter of letters){
+  const letters = sentence.split('');
+  for (let letter of letters) {
     letter = letter.toLowerCase();  //converts to lower case
     if (result[letter]) {
-      result[letter] = result[letter].push(1); //if result is in the object it will add to total
+      result[letter] = result[letter] + 1; //if result is in the object it will add to total
     } else {
-      result[letter] = [1]; //if not in object it will add additional key
+      result[letter] = 1; //if not in object it will add additional key
     }
   }
   return result;
 };
 
 const letterTotals = countLetters(quote);
-console.log(letterTotals);
+console.log(letter);
--- letterPositions.js
+++ letterPositions.js
@@ -1,43 +1,38 @@
-eqArrays = function (a, b) {
+eqArrays = function(a, b){
   if (a instanceof Array && b instanceof Array) {
     if (a.length != b.length)  //ensuring correct length
-      return false;
-    for (let i = 0; i < a.length; i++) //ensuring each element is equal
-      if (!eqArrays(a[i], b[i]))
+    return false;
+  for (let i =0; i< a.length; i++) //ensuring each element is equal
+     if (!eqArrays(a[i], b[i]))
         return (false);
-    return (true);
+  return (true);
 
-  } else {
-    return a === b;
+  }else {
+    return a===b;
   }
 }
 
-assertArraysEqual = function (a, b) {
-  if (eqArrays(a, b)) {
-    console.log("this array is true");
-
-  } else {
-    console.log("this array is false");
-  }
+assertArraysEqual = function (a, b){
+if (eqArrays(a,b)){
+  console.log("this array is true");
+  
+}else {
+  console.log("this array is false");
+}
 
 }
 
+let quote = "Lighthouse Labs";
+quote = quote.replace(/\s+/g, '');  //removes spaces from string
 
+let letterPositions = function(sentence){
+  const results = {};
+for (let i = 0; i < sentence.length; i++)
+if ()
 
-const letterPositions = function (sentence) {
-  let results = {};
-  for (let i = 0; i < sentence.length; i++) {
-    if (results[sentence[i]]) {
-      results[sentence[i]].push(i);
-    
-    } else {
-      results[sentence[i]] = [i];
-    }
-  }
   return results;
 };
 
 
 
-console.log(letterPositions("Hello"));
-console.log(letterPositions("Tomato Potato"));
+console.log(assertArraysEqual(letterPositions("Hello").e, [1]));
