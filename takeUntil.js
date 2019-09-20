09a221244fe9d0b814b2271d29ca2b8596abdb8c
Logan Roberts <lroberts77@gmail.com>
2019-09-20 02:52:13 +0000

    Created a function for takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,36 @@
-// const takeUntil = function(array, callback) {
+const eqArrays = function(a, b){
+  if (a instanceof Array && b instanceof Array) {
+    return JSON.stringify(a) === JSON.stringify(b);
+  }
+
+  return false;
+}
+
+assertArraysEqual = function (a, b){
+if (eqArrays(a === b)){
+  console.log(`😁😁😁 Assertion passed! ${a} === ${b}`);
   
-// }
+}else {
+  console.log(`👺👺👺 Assertion failed! ${a} !== ${b}`);
+}
+
+}
+
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let num  of array){
+    if (callback(num)) {
+      return results;
+    }
+
+    results.push(num)
+  }
+
+  return results;
+    
+  }
+
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
