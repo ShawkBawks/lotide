0919b57611c5fdeaf6c6ad094a7d3e7a1c88637b
Logan Roberts <lroberts77@gmail.com>
2019-09-23 19:58:28 +0000

    initial refactor and module exports/requires

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`\u1F60A, \u1F60A, \u1F60A, Assertion Passed:  ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    return (`\u1F92C, \u1F92C, \u1F92C, Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require (`./assertEqual`);
+
 
 const eqArrays = function(a, b){
   if (a instanceof Array && b instanceof Array) {
@@ -13,9 +8,5 @@ const eqArrays = function(a, b){
 
   return false;
 }
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => true
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)); // => false
 
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
+module.exports = eqArrays;
